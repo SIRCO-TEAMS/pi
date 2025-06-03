5bbad32b1eb5cd31448eb6330770139a9da64b00
SIRCO-TEAMS <sirco.team@gmail.com>
2025-06-03 11:05:24 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+wget https://raw.githubusercontent.com/SIRCO-TEAMS/pi/refs/heads/main/update.sh
+wget https://raw.githubusercontent.com/SIRCO-TEAMS/pi/refs/heads/main/pi.sh
+wget https://raw.githubusercontent.com/SIRCO-TEAMS/pi/refs/heads/main/reconf.sh
+sudo chmod +x update.sh
+sudo chmod +x pi.sh 
+sudo chmod +x reconf.sh
+./update.sh -y
+./pi.sh
+
+echo "****************************************************************************************************************************"
+echo "* Pistop setup complete THANK YOU!!! You can run ./reconf.sh to look at the settings you have used and logs of the setup.  *"
+echo "****************************************************************************************************************************"
