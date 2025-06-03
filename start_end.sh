53e0d66b25c8afe9cfbe7cfea97d7fb6e53c7de3
SIRCO-TEAMS <sirco.team@gmail.com>
2025-06-03 18:50:18 +0000

    fix

--- start_end.sh
+++ start_end.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
 ./update.sh -y
+
+sudo rm update.sh && sudo rm pi.sh && sudo rm start.sh
 
 echo "****************************************************************************************************************************"
 echo "* Pistop setup complete THANK YOU!!! You can run ./reconf.sh to look at the settings you have used and logs of the setup.  *"
