784cce6eb66058ce89b221e2e8c32473ee769452
SIRCO-TEAMS <sirco.team@gmail.com>
2025-06-03 11:27:48 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,10 @@ sudo chmod +x pi.sh
 sudo chmod +x reconf.sh
 ./update.sh -y
 ./pi.sh
+./update.sh -y
 
 echo "****************************************************************************************************************************"
 echo "* Pistop setup complete THANK YOU!!! You can run ./reconf.sh to look at the settings you have used and logs of the setup.  *"
 echo "****************************************************************************************************************************"
+read -p "Press Enter to reboot."
+sudo reboot now
